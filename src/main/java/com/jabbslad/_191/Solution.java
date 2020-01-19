e67ce648580245291582a6158f13e9bafed3d6b8
Jamie Atkinson <jabbslad@gmail.com>
2020-01-19 20:15:08 +0000

    add solution 191

---
+++ src/main/java/com/jabbslad/_191/Solution.java
@@ -0,0 +1,19 @@
+package com.jabbslad._191;
+
+public class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+         for(int i = 0; i < 32; i++) {
+             if ((n & (1 << i)) != 0) {
+                 count++;
+             }
+         }
+         return count;
+    }
+
+    public static void main(String... args) {
+        Solution solution = new Solution();
+        int result = solution.hammingWeight(11);
+        assert result == 3;
+    }
+}
