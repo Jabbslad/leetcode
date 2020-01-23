c7efba4137039998517f4b9e3b3700bb71b6fce4
Jamie Atkinson <jabbslad@gmail.com>
2020-01-23 23:56:48 +0000

    add solution to 942

---
+++ src/main/java/com/jabbslad/_942/Solution.java
@@ -0,0 +1,19 @@
+package com.jabbslad._942;
+
+public class Solution {
+    public int[] diStringMatch(String S) {
+        int N = S.length();
+        int low = 0, high = N;
+
+        int[] res = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            if (S.charAt(i) == 'I') {
+                res[i] = low++;
+            } else {
+                res[i] = high--;
+            }
+        }
+        res[N] = low;
+        return res;
+    }
+}
