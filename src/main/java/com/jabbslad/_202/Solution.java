36059cb8af4b4df0fb093473aca063e2bc286e28
Jamie Atkinson <jabbslad@gmail.com>
2020-01-24 21:11:00 +0000

    add solution to 202

---
+++ src/main/java/com/jabbslad/_202/Solution.java
@@ -0,0 +1,25 @@
+package com.jabbslad._202;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int next(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n /= 10;
+            sum += d * d;
+        }
+        return sum;
+    }
+
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = next(n);
+        }
+        return n == 1;
+    }
+}
